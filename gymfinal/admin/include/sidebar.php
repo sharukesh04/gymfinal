a1980572a5088b1a9591d78a31a72f6e7da0e3c3
sharukesh04 <sharukeshwarsk@gmail.com>
2024-12-06 12:05:00 +0530

    first commit

---
+++ gymfinal/admin/include/sidebar.php
@@ -0,0 +1,28 @@
+<aside class="app-sidebar">
+     
+      <ul class="app-menu">
+        <li><a class="app-menu__item" href="index.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+        
+        
+        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Package</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="add-post.php"><i class="icon fa fa-circle-o"></i>Add</a></li>
+            <li><a class="treeview-item" href="manage-post.php"><i class="icon fa fa-circle-o"></i> Manage</a></li>
+          </ul>
+        </li>
+
+
+         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Bookings</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="new-bookings.php"><i class="icon fa fa-circle-o"></i>New</a></li>
+
+            <li><a class="treeview-item" href="booking-history.php"><i class="icon fa fa-circle-o"></i> All</a></li>
+          </ul>
+        </li>
+
+        
+     
+
+          
+      </ul>
+    </aside>
---
+++ gymfinal/include/footer.php
@@ -0,0 +1,16 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">
+						<div class="copyright"> 
+GYM Managaement System</div>
+<div class="admin">
+<a href="admin/login.php">Admin</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
